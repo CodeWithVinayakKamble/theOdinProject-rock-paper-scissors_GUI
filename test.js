c66c08d6e1f8d07ecc39e966049fe069cc38219c
Vinayak Kamble <110600027+CodeWithVinayakKamble@users.noreply.github.com>
2026-06-20 10:24:19 +0530

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const getComputerChoice = () => {
-    const compOption = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * compOption.length)
-    const computerChoice = compOption[randomIndex];
-    return computerChoice;
-}
